7d053ca599c840c9cab235f2b116838010bb6bcb
oskg <userosss001@gmail.com>
2022-03-31 19:43:30 +0300

    Added fin tests for lab1

--- modules/complex-number/test/test_Oskin_Georgii_complex_number.cpp
+++ modules/complex-number/test/test_Oskin_Georgii_complex_number.cpp
@@ -31,3 +31,38 @@ TEST(Oskin_Georgii_ComplexNumTest, Init_by_Constructor) {
     EXPECT_EQ(cn_2.getIm(), 1.0);
 }
 
+TEST(Oskin_Georgii_ComplexNumTest, ComplexNum_Comparsion_unInit) {
+    // Arrange
+    double re = 3.0;
+    double im = 2.0;
+
+    // Act
+    ComplexNumber cn_1(re, im);
+    ComplexNumber cn_2;
+    bool tmp_1 = (cn_1 == cn_2);
+    cn_2 = cn_1;
+    bool tmp_2 = (cn_1 == cn_2);
+
+    // Assert
+    EXPECT_EQ(0, tmp_1);
+    EXPECT_EQ(1, tmp_2);
+}
+
+TEST(Oskin_Georgii_ComplexNumTest, ComplexNum_Comparsion_Init) {
+    // Arrange
+    double re_1 = 3.0;
+    double im_1 = 2.0;
+    double re_2 = 4.0;
+    double im_2 = 5.0;
+
+    // Act
+    ComplexNumber cn_1(re_1, im_1);
+    ComplexNumber cn_2(re_2, im_2);
+    bool tmp_1 = (cn_1 == cn_2);
+    cn_2 = cn_1;
+    bool tmp_2 = (cn_1 == cn_2);
+
+    // Assert
+    EXPECT_EQ(0, tmp_1);
+    EXPECT_EQ(1, tmp_2);
+}
