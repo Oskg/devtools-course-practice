98503d331c2ef252186d0731ec25e79f2e81c083
oskg <userosss001@gmail.com>
2022-03-31 19:30:44 +0300

    Added new tests for lab1

--- modules/complex-number/test/test_Oskin_Georgii_complex_number.cpp
+++ modules/complex-number/test/test_Oskin_Georgii_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Oskin_Georgii_ComplexNumTest, Can_Create_Zero) {
+TEST(Oskin_Georgii_ComplexNumTest, Zero_Init) {
     // Arrange
     double re = 0.0;
     double im = 0.0;
@@ -16,3 +16,18 @@ TEST(Oskin_Georgii_ComplexNumTest, Can_Create_Zero) {
     ASSERT_NO_THROW(z.getRe());
     ASSERT_NO_THROW(z.getIm());
 }
+
+TEST(Oskin_Georgii_ComplexNumTest, Init_by_Constructor) {
+    // Arrange
+    double re = 3.0;
+    double im = 1.0;
+
+    // Act
+    ComplexNumber cn_1(re, im);
+    ComplexNumber cn_2 = cn_1;
+
+    // Assert
+    EXPECT_EQ(cn_2.getRe(), 3.0);
+    EXPECT_EQ(cn_2.getIm(), 1.0);
+}
+
